cb8ca3c3e7363afe2f343ad19051cc64ce8edfba
pj_max <vjiangcg@gmail.com>
2022-12-14 14:09:15 -0500

    update nav bar

--- ccs-react/src/Components/Nav/Nav.js
+++ ccs-react/src/Components/Nav/Nav.js
@@ -36,7 +36,7 @@ export default function Nav() {
         <body class="h-full">
         ```
       */}
-      <div className="min-h-full">
+      <div className="min-h-0">
         <Disclosure as="nav" className="bg-gray-800">
           {({ open }) => (
             <>
@@ -183,21 +183,6 @@ export default function Nav() {
             </>
           )}
         </Disclosure>
-
-        <header className="bg-white shadow">
-          <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-          </div>
-        </header>
-        <main>
-          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            {/* Replace with your content */}
-            <div className="px-4 py-6 sm:px-0">
-              <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
-            </div>
-            {/* /End replace */}
-          </div>
-        </main>
       </div>
     </>
   )
